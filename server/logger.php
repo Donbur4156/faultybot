71ac48495569dd3778a52b99ce9d4e71dba09e12
Jonathan Skopp <zeyecx@gmail.com>
2021-04-04 15:31:24 +0200

    Fix CSV Handler

--- server/logger.php
+++ server/logger.php
@@ -5,7 +5,7 @@
  */
 function logger($team){
    create_logger_file();
-   $datei = fopen("log.csv", "a+");
+   $datei = fopen("log.txt", "a+");
    $ip = $_SERVER["REMOTE_ADDR"];  
    $host = gethostbyaddr($ip);
    $datum = date("d.m.Y",time());
@@ -20,8 +20,8 @@ function logger($team){
       };
 
 
-   $data = array($ip,$host,$datum,$uhrzeit,$token,strval($team));
-   fputcsv($datei, explode(";",$data));
+   $data = $ip.";".$host.";".$datum.";".$uhrzeit.";".$token.";".strval($team)."\n";
+   fwrite($datei, $data);
    fclose($datei);
 }
 
@@ -30,13 +30,12 @@ function logger($team){
  *  create logger file
  */ 
 function create_logger_file(){
-    if (!file_exists('log.csv')) {   
-      touch('log.csv');
-      $datei = fopen("log.csv", "a+");
-      $data = array("IP","Host","Datum","Uhrzeit","Token","Team");
-      fputcsv($datei, explode(";",$data));
+    if (!file_exists('log.txt')) {   
+      // touch('log.txt');
+      $datei = fopen("log.txt", "a+");
+      $data =  "IP; Host; Datum; Uhrzeit; Token; Team \n"   ;
+      fwrite($datei, $data);
       fclose($datei);
         }
-
 }
 ?>
