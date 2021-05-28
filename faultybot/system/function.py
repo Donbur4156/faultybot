8149fb5f89d1845219e552bde7dc98d1ae460582
Zeyecx <zeyecx@gmail.com>
2021-05-28 14:03:56 +0200

    first pylint fixes

--- faultybot/system/function.py
+++ faultybot/system/function.py
@@ -6,6 +6,7 @@ import requests
 
 # Checks if a player has violated the rules of Lichess.
 def check_user(username):
+    """ Check User """
     flag = False
     try:
         # The system marks such players with a TOS mark. This can be obtained as an array from the API.
@@ -19,6 +20,7 @@ def check_user(username):
 # Checks the user and outputs whether he has cheated.
 # Since python has a problem with boolean values, this function outputs them as strings.
 def check_user_ausgabe(username):
+    """ Check User Ausgabe """
     if check_user(username):
         return "True"
     else:
@@ -63,6 +65,7 @@ def runner(team, token):
 # Unfortunately, the API returns only an array.
 # This is checked here.
 def check(level):
+    """ check """
     # Since it is a request response, it cannot be interpreted as a string.
     if "true" in level.text:
         return True
@@ -72,6 +75,7 @@ def check(level):
 
 # The function investigates why a request failed.
 def status(level):
+    """ get status """
     if "true" not in level.text:
         # The Lichess API actually works very well.
         # Therefore either the token is wrong or the error is about one meter behind the screen.
@@ -105,14 +109,14 @@ def clear_team(team, token):
 def check_team_name(team):
     # The program uses the static links from lichess
     if "/team/" in team:
-        x = len(team)
-        while x > 0:
+        runner = len(team)
+        while runner > 0:
             # Lichess cannot process teams with the "/" character for syntax reasons.
             # Therefore there are no such teams.
-            if "/" in team[-x::]:
-                x = x - 1
+            if "/" in team[-runner::]:
+                runner = runner - 1
             else:
-                return team[-x::]
+                return team[-runner::]
     else:
         return team
     # The false can never be reached. It stands so that it looks better
