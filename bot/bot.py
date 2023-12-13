445abea603e86d5c3556a14623e63c5a6a94abb2
eskopp <skopp.erik@gmail.com>
2023-12-13 14:41:15 +0100

    Format the code with black
    https://github.com/psf/black

--- bot/bot.py
+++ bot/bot.py
@@ -10,19 +10,29 @@ import config as c
 import function
 import interactions as di
 import lichesspy.api
-from interactions import (Intents, SlashContext, Task, TimeTrigger, listen,
-                          slash_command, slash_option)
+from interactions import (
+    Intents,
+    SlashContext,
+    Task,
+    TimeTrigger,
+    listen,
+    slash_command,
+    slash_option,
+)
 from util.color import Colors
 from util.decorator import teamname_option
 from util.logger import create_logger
 
-di_logger = create_logger(file_name=c.logdir + "interactions.log", log_name="interactions_logger")
+di_logger = create_logger(
+    file_name=c.logdir + "interactions.log", log_name="interactions_logger"
+)
 bot_logger = create_logger(file_name=c.logdir + c.logfilename, log_name="bot_logger")
 
 # Build the bot according to the Discord syntax
 client = di.Client(token=c.bot_token, intents=Intents.ALL, logger=di_logger)
 
 TEAM_FILE = os.path.join(sys.path[0], "teams_to_check.json")
+
 
 # Test function to see if the bot is online.
 @listen()
@@ -33,11 +43,15 @@ async def on_startup():
 
 @slash_command(description="kick faulty user of a team. Authentification needed!")
 @teamname_option()
-@slash_option(name="oauth", description="OAuth Code", opt_type=di.OptionType.STRING, required=True)
+@slash_option(
+    name="oauth", description="OAuth Code", opt_type=di.OptionType.STRING, required=True
+)
 async def kickfaulty(ctx: SlashContext, teamname: str, oauth: str):
     teamname = function.check_team_name(teamname.lower())
-    text = f"The data of the team **{teamname}** is downloaded and checked!\nThis can take several " \
-           f"minutes depending on the size of the team.\nPer 1000 members approx 1 minute!"
+    text = (
+        f"The data of the team **{teamname}** is downloaded and checked!\nThis can take several "
+        f"minutes depending on the size of the team.\nPer 1000 members approx 1 minute!"
+    )
     await ctx.send(embed=_embed(text), ephemeral=True)
     embed, cheaters = await faultyhandle(teamname)
     await ctx.send(embed=embed, ephemeral=True)
@@ -49,50 +63,59 @@ async def kickfaulty(ctx: SlashContext, teamname: str, oauth: str):
 @teamname_option()
 async def faulty(ctx: SlashContext, teamname: str):
     teamname = function.check_team_name(teamname.lower())
-    text = f"The data of the team **{teamname}** is downloaded and checked!\n" \
-            "This can take several minutes depending on the size of the team.\n" \
-            "Per 1000 members approx 1 minute!"
+    text = (
+        f"The data of the team **{teamname}** is downloaded and checked!\n"
+        "This can take several minutes depending on the size of the team.\n"
+        "Per 1000 members approx 1 minute!"
+    )
     await ctx.send(embed=_embed(text, Colors.YELLOW))
     embed, cheaters = await faultyhandle(teamname)
     await ctx.send(embed=embed)
-    
+
+
 cron_cmds = di.SlashCommand(name="cron", description="Commands für den Cronjob")
+
 
 @cron_cmds.subcommand(sub_cmd_name="add", sub_cmd_description="add a team to cron list")
 @teamname_option()
-@slash_option(name="user", description="user who will be pinged", opt_type=di.OptionType.USER, required=True)
+@slash_option(
+    name="user",
+    description="user who will be pinged",
+    opt_type=di.OptionType.USER,
+    required=True,
+)
 async def crone_add(ctx: SlashContext, teamname: str, user: di.Member):
     teamname = function.check_team_name(teamname.lower())
-    bot_logger.info(f'Team {teamname} for User {user.id}')
-    with open(TEAM_FILE, 'r', encoding='utf-8') as json_file:
+    bot_logger.info(f"Team {teamname} for User {user.id}")
+    with open(TEAM_FILE, "r", encoding="utf-8") as json_file:
         json_data: dict[str, list[dict[str, list[str]]]] = json.load(json_file)
     teamnew = True
-    for team in json_data['teams']:
-        if team['teamname'] == teamname:
-            if str(user.id) not in team['user']:
-                team['user'].append(str(user.id))
+    for team in json_data["teams"]:
+        if team["teamname"] == teamname:
+            if str(user.id) not in team["user"]:
+                team["user"].append(str(user.id))
             teamnew = False
             break
     if teamnew:
-        json_data['teams'].append({
-            'teamname': teamname,
-            'user': [str(user.id)]
-        })
-    with open(TEAM_FILE, 'w', encoding='utf-8') as json_file:
+        json_data["teams"].append({"teamname": teamname, "user": [str(user.id)]})
+    with open(TEAM_FILE, "w", encoding="utf-8") as json_file:
         json.dump(json_data, json_file)
     text = f"Das Team **{teamname}** ist nun mit dem User {user.mention} verknüpft."
     await ctx.send(embed=_embed(text))
 
 
-@cron_cmds.subcommand(sub_cmd_name="get_cron_teams", sub_cmd_description="get all teams which are checked daily")
+@cron_cmds.subcommand(
+    sub_cmd_name="get_cron_teams",
+    sub_cmd_description="get all teams which are checked daily",
+)
 async def cron_get(ctx: SlashContext):
-    with open(TEAM_FILE, 'r', encoding='utf-8') as json_file:
+    with open(TEAM_FILE, "r", encoding="utf-8") as json_file:
         json_data = json.load(json_file)
     text = ""
-    for team in json_data['teams']:
-        teamname = team['teamname']
-        userlist = team['user']
-        text += f'\nDas Team **{teamname}** ist mit diesen Usern verknüpft:\n'
+    for team in json_data["teams"]:
+        teamname = team["teamname"]
+        userlist = team["user"]
+        text += f"\nDas Team **{teamname}** ist mit diesen Usern verknüpft:\n"
         for user in userlist:
             text += f"<@{str(user)}> "
     await ctx.send(embed=_embed(text, Colors.BLUE), allowed_mentions=None)
@@ -101,19 +124,25 @@ async def cron_get(ctx: SlashContext):
 async def faultyhandle(team: str):
     try:
         loop = asyncio.get_event_loop()
-        cheaters = await loop.run_in_executor(ThreadPoolExecutor(), function.analyse_team, team, c.ignore_user)
+        cheaters = await loop.run_in_executor(
+            ThreadPoolExecutor(), function.analyse_team, team, c.ignore_user
+        )
     except lichesspy.api.ApiHttpError:
         text = f"The queried team **{team}** apparently does not exist! \n"
         embed = _embed(text, Colors.RED)
         return embed, None
     if not cheaters:
         text = f"The queried team **{team}** does not include flagged users!\n"
-        embed= _embed(text)
+        embed = _embed(text)
         return embed, None
-    data = "\n".join([f"[{cheater}](https://lichess.org/@/{cheater})" for cheater in cheaters])
-    text = f"In the team **{team}**, " \
-           f"following users were marked by Lichess as having violated the terms of use:\n" \
-           f"{data}\n"
+    data = "\n".join(
+        [f"[{cheater}](https://lichess.org/@/{cheater})" for cheater in cheaters]
+    )
+    text = (
+        f"In the team **{team}**, "
+        f"following users were marked by Lichess as having violated the terms of use:\n"
+        f"{data}\n"
+    )
     embed = _embed(text, Colors.ORANGE)
     return embed, cheaters
 
@@ -123,11 +152,15 @@ async def run_kick(ctx: SlashContext, team: str, cheaters: list, token: str):
     count_cheater = 0
     for cheater in cheaters:
         request = function.kick(team.lower(), cheater, token)
-        bot_logger.info(f"Request for Cheater {count_cheater + 1}. {cheater}: returns {request.text}")
+        bot_logger.info(
+            f"Request for Cheater {count_cheater + 1}. {cheater}: returns {request.text}"
+        )
         if not function.check(request):
             status = function.status(request)
-            text = f"The kick process was cancelled due to the following error:\n**{status}**" \
-                    "\nFor further information, please contact donbur#4156 on discord!"
+            text = (
+                f"The kick process was cancelled due to the following error:\n**{status}**"
+                "\nFor further information, please contact donbur#4156 on discord!"
+            )
             bot_logger.info(f"Request failed with {status}: ({request})")
             await ctx.send(embed=_embed(text, Colors.RED), ephemeral=True)
             break
@@ -145,38 +178,51 @@ async def run_kick(ctx: SlashContext, team: str, cheaters: list, token: str):
 async def cron_faulty():
     bot_logger.info("start Crown")
     request_channel = await client.fetch_channel(channel_id=c.cron_channel)
-    with open(TEAM_FILE, 'r', encoding='utf-8') as json_file:
+    with open(TEAM_FILE, "r", encoding="utf-8") as json_file:
         json_data = json.load(json_file)
-    for team in json_data['teams']:
-        teamname = team['teamname']
-        userlist = team['user']
+    for team in json_data["teams"]:
+        teamname = team["teamname"]
+        userlist = team["user"]
         bot_logger.info(f'Team "{teamname}" for {userlist}')
         try:
             loop = asyncio.get_event_loop()
             cheaters = await loop.run_in_executor(
-                ThreadPoolExecutor(), function.analyse_team, 
-                teamname, c.ignore_user)
+                ThreadPoolExecutor(), function.analyse_team, teamname, c.ignore_user
+            )
         except lichesspy.api.ApiHttpError:
             bot_logger.warn(f"Cron Request for Team {teamname} failed.")
             return False
         if cheaters:
-            data = "\n".join([f"[{cheater}](https://lichess.org/@/{cheater})" for cheater in cheaters])
-            bot_logger.info(f'{str(len(cheaters))} cheater found in team "{teamname}": {cheaters}')
-            user_mentions = ", ".join([f'<@{user}>' for user in userlist])
-            text = f"In the team **{teamname}**, " \
+            data = "\n".join(
+                [
+                    f"[{cheater}](https://lichess.org/@/{cheater})"
+                    for cheater in cheaters
+                ]
+            )
+            bot_logger.info(
+                f'{str(len(cheaters))} cheater found in team "{teamname}": {cheaters}'
+            )
+            user_mentions = ", ".join([f"<@{user}>" for user in userlist])
+            text = (
+                f"In the team **{teamname}**, "
                 f"following users were marked by Lichess as having violated the terms of use:\n{data}\n"
-            await request_channel.send(content=user_mentions, embed=_embed(text, Colors.ORANGE))
+            )
+            await request_channel.send(
+                content=user_mentions, embed=_embed(text, Colors.ORANGE)
+            )
     bot_logger.info("end Crown")
 
 
 def create_teamlist():
     if not os.path.isfile(TEAM_FILE):
-        with open(TEAM_FILE, 'w', encoding='utf-8') as jsonfile:
-            data = {'teams': []}
+        with open(TEAM_FILE, "w", encoding="utf-8") as jsonfile:
+            data = {"teams": []}
             json.dump(data, jsonfile)
+
 
 def _embed(text, color=Colors.GREEN):
     return di.Embed(description=text, color=color)
+
 
 # Starting from the Await Client
 if __name__ == "__main__":
