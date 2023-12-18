6012561e976563e4d2877db17af3516aaf037b11
eskopp <skopp.erik@gmail.com>
2023-12-18 22:41:33 +0100

    Create Doc Strings

--- bot/util/decorator.py
+++ bot/util/decorator.py
@@ -1,11 +1,28 @@
+"""
+decorator.py
+
+This module defines utility functions for creating slash command options.
+"""
+
 from interactions import OptionType, slash_option
 
 
 def teamname_option():
+    """
+    A decorator for adding a "teamname" option to a function.
+
+    This option is typically used in Discord slash commands.
+
+    Usage:
+        @teamname_option()
+        async def my_command(ctx, teamname):
+            # Your command logic here
+    """
+
     def wrapper(func):
         return slash_option(
             name="teamname",
-            description="teamname to check",
+            description="Teamname to check",
             opt_type=OptionType.STRING,
             required=True,
         )(func)
